5e83186e921a27c45a1268f143972dd4dc4dbd1d
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-13 23:17:05 +0300

    Сreated mock data component

---
+++ js/components/widgets/download/data/Data.js
@@ -0,0 +1,24 @@
+export const data = {
+  download:{
+  title:{
+    content: 'Peaceful shopping mindful money',
+    priority: 1,
+  },
+  text:'See how we can help with making your shopping experience and money management more ZEN.',
+  links:{
+    google_link:{
+      src:'https://play.google.com',
+      description: 'link to google play',
+    },
+    apple_link:{
+      src:'https://www.apple.com/store',
+      description: 'link to apple store',
+    },
+  },
+  image:{
+    src: './../../../../../assets/img/download_image.svg',
+    description: 'image for download section',
+  }
+  }, 
+}
+     
