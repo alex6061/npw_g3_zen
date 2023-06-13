14ab748fb0caca6a98f8ae7f3cb203550576f897
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-13 23:17:05 +0300

    Сreated mock data component
    Fixed Formated document
    
    Fix
    
    chenged image to jpg
    
    Fix
    
    Fix
    
    Changed the name of the text key to texts and replaced the descriptions
    
    chenged "src to url"

---
+++ js/data/data.js
@@ -0,0 +1,25 @@
+export const data = {
+  download: {
+    title: {
+      content: 'Peaceful shopping mindful money',
+      priority: 1,
+    },
+    texts: [
+      'See how we can help with making your shopping experience and money management more ZEN.',
+    ],
+    links: {
+      googleLink: {
+        url: 'https://play.google.com',
+        description: 'google',
+      },
+      appleLink: {
+        url: 'https://www.apple.com/store',
+        description: 'apple',
+      },
+    },
+    image: {
+      url: 'assets/img/1.jpg',
+      description: 'image for download section',
+    },
+  },
+};
