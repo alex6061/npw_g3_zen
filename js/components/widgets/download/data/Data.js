286765455eb06f8aa15d1e3218b81e44b0eb4fcb
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-14 07:21:58 +0300

    Fixed Formated document

--- js/components/widgets/download/data/Data.js
+++ js/components/widgets/download/data/Data.js
@@ -1,24 +1,23 @@
 export const data = {
-  download:{
-  title:{
-    content: 'Peaceful shopping mindful money',
-    priority: 1,
-  },
-  text:'See how we can help with making your shopping experience and money management more ZEN.',
-  links:{
-    google_link:{
-      src:'https://play.google.com',
-      description: 'link to google play',
+  download: {
+    title: {
+      content: "Peaceful shopping mindful money",
+      priority: 1,
     },
-    apple_link:{
-      src:'https://www.apple.com/store',
-      description: 'link to apple store',
+    text: "See how we can help with making your shopping experience and money management more ZEN.",
+    links: {
+      google_link: {
+        src: "https://play.google.com",
+        description: "link to google play",
+      },
+      apple_link: {
+        src: "https://www.apple.com/store",
+        description: "link to apple store",
+      },
+    },
+    image: {
+      src: "./../../../../../assets/img/download_image.svg",
+      description: "image for download section",
     },
   },
-  image:{
-    src: './../../../../../assets/img/download_image.svg',
-    description: 'image for download section',
-  }
-  }, 
-}
-     
+};
