c1a199bd07a171c4b170c22a533735cedf9112b1
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-14 21:59:54 +0300

    Fix

--- js/components/widgets/download/data/Data.js
+++ js/components/widgets/download/data/Data.js
@@ -1,25 +1,25 @@
 export const data = {
   download: {
     title: {
-      content: "Peaceful shopping mindful money",
+      content: 'Peaceful shopping mindful money',
       priority: 1,
     },
     text: [
-      "See how we can help with making your shopping experience and money management more ZEN.",
+      'See how we can help with making your shopping experience and money management more ZEN.',
     ],
     links: {
       googleLink: {
-        src: "https://play.google.com",
-        description: "link to google play",
+        src: 'https://play.google.com',
+        description: 'link to google play',
       },
-      apple_link: {
-        src: "https://www.apple.com/store",
-        description: "link to apple store",
+      appleLink: {
+        src: 'https://www.apple.com/store',
+        description: 'link to apple store',
       },
     },
     image: {
-      src: "assets/img/1.jpg",
-      description: "image for download section",
+      src: 'assets/img/1.jpg',
+      description: 'image for download section',
     },
   },
 };
