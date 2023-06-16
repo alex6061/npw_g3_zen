7257f66a5ea97b57ace29a13bb9d706630ce82a1
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-16 19:38:59 +0300

    Fixed all edits

--- js/components/widgets/Title.js
+++ js/components/widgets/Title.js
@@ -21,11 +21,11 @@ export const Title = (data, parrentClassName) => {
     : 'title';
 
   switch(priority) {
-    default: return `<h1 class="${currentClassName}>${content}</h1>"`;
-    case 2: return `<h2 class="${currentClassName}>${content}</h2>"`;
-    case 3: return `<h3 class="${currentClassName}>${content}</h3>"`;
-    case 4: return `<h4 class="${currentClassName}>${content}</h4>"`;
-    case 5: return `<h5 class="${currentClassName}>${content}</h5>"`;
-    case 6: return `<h6 class="${currentClassName}>${content}</h6>"`;
-  }
-}
+    default: return `<h1 class="${currentClassName}">${content}</h1>`;
+    case 2: return `<h2 class="${currentClassName}">${content}</h2>`;
+    case 3: return `<h3 class="${currentClassName}">${content}</h3>`;
+    case 4: return `<h4 class="${currentClassName}">${content}</h4>`;
+    case 5: return `<h5 class="${currentClassName}">${content}</h5>`;
+    case 6: return `<h6 class="${currentClassName}">${content}</h6>`;
+  };
+};
