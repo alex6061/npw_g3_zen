5e30ae947739179eb114b7d2bef9f53259f295df
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-18 14:29:04 +0300

    clear branch

--- js/components/widgets/Link.js
+++ js/components/widgets/Link.js
@@ -3,17 +3,17 @@
  * @typedef {Object} Data
  * @property {string} type
  * @property {string} url
- * @param {Data} data 
- * @param {string} parrentClassName 
- * @returns {string} HTML or empty 
+ * @param {Data} data
+ * @param {string} parrentClassName
+ * @returns {string} HTML or empty
  */
 
 export const Link = (data, parrentClassName) => {
-  if (!Object.keys(data).length === 2) return '';
+  if (!Object.keys(data).length === 2) return "";
   const { type, link } = data;
 
-  const currentClassName = parrentClassName 
-    ? `${parrentClassName}__link` 
+  const currentClassName = parrentClassName
+    ? `${parrentClassName}__link`
     : `link`;
 
   return `
@@ -21,7 +21,7 @@ export const Link = (data, parrentClassName) => {
       class="${currentClassName}"
       src="${url}"
     >
-      ${type === 'google' ? IconGoogle() : IconApple()}
+      ${type === "google" ? IconGoogle() : IconApple()}
     </a>
   `;
 };
