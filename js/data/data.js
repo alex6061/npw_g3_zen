921be04bb7fb9ec104b8e50a2da308f7e05e4634
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-22 23:12:16 +0300

    WIP

--- js/data/data.js
+++ js/data/data.js
@@ -4,7 +4,7 @@ export const data = {
       content: 'Peaceful shopping mindful money',
       priority: 1,
     },
-    texts: [
+    texts: [ 
       'See how we can help with making your shopping experience and money management more ZEN.',
     ],
     links: {
---
+++ js/data/widgets/Text.js
@@ -0,0 +1,5 @@
+
+
+const Text = (text, parrentClassName) => {
+  ()
+}
