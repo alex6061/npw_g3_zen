ae4c738dff2afc236d05717d70fa4df5ea877f46
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-23 09:37:19 +0300

    Added destructuring

--- js/components/widgets/Image.js
+++ js/components/widgets/Image.js
@@ -14,7 +14,7 @@
 export const Image = (data, parrentClassName) => {
   if (Object.keys(data).length !== 2) return '';
 
-  const { url } = data
+  const { url, description } = data.links;
 
   const currentClassName = parrentClassName 
     ? `${parrentClassName}__image` 
