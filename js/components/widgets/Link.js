732e61b234f489d6ffb300b43c9e4f606cab9a71
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-23 09:03:15 +0300

    WIP

--- js/components/widgets/Link.js
+++ js/components/widgets/Link.js
@@ -1,15 +1,18 @@
 /**
- * @function Link
  * @typedef {Object} Data
  * @property {string} type
  * @property {string} url
+ */
+
+/**
+ * @function Link
  * @param {Data} data
  * @param {string} parrentClassName
  * @returns {string} HTML or empty
  */
 
 export const Link = (data, parrentClassName) => {
-  if (!Object.keys(data).length !== 2) return '';
+  if (Object.keys(data).length !== 2) return '';
   const { type, url } = data;
 
   const currentClassName = parrentClassName
