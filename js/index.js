d765b5788a9b227355c31998a0b7c077de0fa17a
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-27 21:59:06 +0300

    WIP Section Primary

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log('start');
+import { data } from './data/data.js';
+import { SectionPrimary } from './widgets/SectionPrimary/SectionPrimary.js';
+
+const {download} = data;
+
+const $root = document.querySelector('#root');
+console.log($root)
+
+$root.insertAdjacentHTML('beforeend', );
