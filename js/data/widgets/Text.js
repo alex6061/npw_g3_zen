9e490b20f291ee356b1a0f7e4f8e50b63ab9020a
a.garkusha <a.garkusha@itcsolutions.ru>
2023-06-27 23:27:01 +0300

    Corrected the Text component

--- js/data/widgets/Text.js
+++ js/data/widgets/Text.js
@@ -1,24 +1,26 @@
+import data from './../data.js';
+
 /**
  * @typedef {Object} text
  */
 
 /**
  * @function Text
- * @param {string} text 
+ * @param {data} text
  * @param {string} parrentClassName 
  * @returns {string} Html or Empty
  */
 
-const Text = (texts, parrentClassName) => {
+ export const Text = (texts, parrentClassName) => {
   if(Object.keys(texts) !== 2) return '';
 
   const currentClassName = parrentClassName 
     ? `${parrentClassName}__text` 
     : 'text';
-
-  return `
-    <p class="${currentClassName}">
-      ${Text}
-    </p>
+    
+    return `
+      <p class="${currentClassName}">
+        ${texts.length > 0 ? texts.map((text) => Text(text, className)).join('') : ''}
+      </p>
   `;
-};
+}
