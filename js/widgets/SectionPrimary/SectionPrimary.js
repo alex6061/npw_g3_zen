0c58f5478e3c6a532bd6ed04b0372798af2d017b
a.garkusha <a.garkusha@itcsolutions.ru>
2023-07-03 00:00:26 +0300

    Added the SectionPrimary component and typing for it

---
+++ js/widgets/SectionPrimary/SectionPrimary.js
@@ -0,0 +1,33 @@
+import{Title} from './../../components/Title/Title.js'
+import{Link} from './../../components/Link/Link.js'
+// import{Image} from './../../components/Image/Image.js' доделать
+
+/**@typedef {import('./SectionPrimary.types').SectionPrimaty} Data  */
+
+/**
+ * @function SectionPrimary
+ * @param {Data} data
+ * @returns {string} 
+ */
+
+
+const SectionPrimary = (data) => {
+  const dataKeys = Object.keys(data);
+
+  if(dataKeys.length !== 4 || dataKeys.length !== 5) return '';
+
+  const{name, title, texts, links, image} = data;
+
+  const className = name
+    ? name
+    : 'section-primary';
+
+  return `
+    <section class="${className}">
+      ${title ? title(title, className) : ''}
+      ${texts.length > 0 ? texts.map((text) => Text(text,className)).join('') : ''}
+      ${image ? Image(image, className)  : ''}
+      ${links.length > 0 ? links.map((link) => Link(link, className)).join('') : ''}
+    </section>
+  `
+}
