9cbe8bcd8fee7cb0699c75849ebe853f8b7be18a
a.garkusha <a.garkusha@itcsolutions.ru>
2023-07-05 21:23:41 +0300

    Created data component for eng and ru Language

--- js/data/data.js
+++ js/data/data.js
@@ -1,25 +1,668 @@
 export const data = {
-  download: {
-    title: {
-      content: 'Peaceful shopping mindful money',
-      priority: 1,
-    },
-    texts: [
-      'See how we can help with making your shopping experience and money management more ZEN.',
-    ],
-    links: {
-      googleLink: {
-        url: 'https://play.google.com',
-        description: 'google',
+  en: {
+    header: {
+      logo: {
+        sourse: './../../img/logo.png',
+        description:'footer__icon',
       },
-      appleLink: {
-        url: 'https://www.apple.com/store',
-        description: 'apple',
+      menuItems: [
+        {
+          target: 'download',
+          content: 'Download',
+          type: 'anchor'
+        },
+        {
+          target: 'warranty',
+          content: 'Warranty',
+          type: 'anchor' 
+        },
+        {
+          target: 'care',
+          content: 'Care',
+          type: 'anchor' 
+        },
+        {
+          target: 'clients',
+          content: 'Clients',
+          type: 'anchor' 
+        },
+      ],
+    },
+    download: {
+      name: 'download',
+      title: {
+        content: 'Peaceful shopping mindful money',
+        priority: 1,
+      },
+      texts: [
+        'See how we can help with making your shopping experience and money management more ZEN.',
+      ],
+      links: {
+        googleLink: {
+          url: 'https://play.google.com',
+          description: 'google',
+        },
+        appleLink: {
+          url: 'https://www.apple.com/store',
+          description: 'apple',
+        },
+      },
+      image: {
+        sourse: 'assets/img/1.jpg',
+        description: 'image for download section',
       },
     },
-    image: {
-      url: 'assets/img/1.jpg',
-      description: 'image for download section',
+    warranty: {
+      name: 'warranty',
+      title: {
+        priority: 2,
+        content: '1-year warranty boost shopping mindful money',
+      },
+      texts: [
+        'ZEN cards have a very neat feature that will help you whenever your electronic devices start failing after the standard warranty ends, which is pretty common.',
+        'When you buy any electronics with your ZEN shopping Mastercard, their warranty gets extended by one year. How does it work? It simply does. Automatically.',
+      ],
+      image: {
+        sourse: './../../assets/img/2.jpg',
+        description: 'warranty__image',
+      }
     },
+    care: {
+      name: 'care',
+      title: {
+        priority: 3,
+        content: 'ZEN Care',
+      },
+      texts: [
+        'Shopping online is usually a delightful experience. Until it\'s not',
+        'Until something you buy, proves to be faulty, missing or a totally wrong item, you\’re in for long product return procedures.',
+        'But, here comes ZEN Care: a free, built-in shopping protection.',
+      ],
+      image: {
+        source: './../../assets/img/3.jpg',
+        description: '!!!!!!!! ',
+      }
+    },
+    cashback: {
+      name: 'cashback',
+      title: {
+        priority: 4,
+        content: 'and cashback!',
+      },
+      texts: [
+        'It saves a little bit of money while you’re shopping, but in many cases it takes ages to get anything out of it.',
+        'We gave it a thought and made cashback way more friendly.',
+        'Up to 14% of what you spend using ZEN Mastercard ® will materialize on your account right after the purchase.'   
+      ],
+      button:{
+        title:'Order a consultation'
+      } 
+    },
+    clients:  {
+      name: 'clients',
+      title: {
+        priority: 5,
+        content: '',
+      },
+      clientsList: [
+        {
+          sourse: './../../img/brands/Group-1.svg',
+          description: 'RoseGal'
+        },
+        {
+          sourse:'./../../img/brands/Group-2.svg',
+          description:'GearBest',
+        } ,
+        {
+          sourse:'./../../img/brands/Group-3.svg',
+          description:'Alegro',
+        },
+        {
+          sourse:'./../../img/brands/Group-4.svg',
+          description: 'AliExpress',
+        },
+        {
+          sourse:'./../../img/brands/Group-5.svg',
+          description:'Booking.com',
+        } ,
+        {
+          sourse:'./../../img/brands/Group-6.svg',
+          description:'MediaMarkt',
+        },
+        {
+          sourse:'./../../img/brands/Group-7.svg',
+          description:'RTV',
+        },
+        {
+          sourse:'./../../img/brands/Group-8.svg',
+          description:'Groupon',
+        },
+      ],
+    },
+    footer: {
+      columns: [
+        {
+          title: 'Learn',
+          links:  [
+            {
+              url: 'https://example.com',
+              content: 'Help center'
+            },
+            {
+              url: '#',
+              content:  'Join ZEN team',
+            },
+            {
+              url: '#',
+              content: 'Help center'
+            },
+            {
+              url: '#',
+              content: 'Privacy Policy'
+            },
+            {
+              url: '#',
+              content: 'Cookies'
+            },
+            {
+              url: '#',
+              content: 'Terms of use'
+            },
+          ]
+        },
+        {
+          title: 'Discover',
+          links:  [
+            {
+              url: '#',
+              content:  'For developers',
+            },
+            {
+              url: '#',
+              content:  'Apple Pay',
+            },
+            {
+              url: '#',
+              content:  'Google Pay',
+            }
+          ]
+        },
+        {
+          title: 'Offer',
+          links:  [
+            {
+              url: '#',
+              content:  'Business',
+            },
+            {
+              url: '#',
+              content:  'Personal',
+            },
+            {
+              url: '#',
+              content:  'Privacy Policy',
+            },
+            {
+              url: '#',
+              content:  'Cookies',
+            },
+            {
+              url: '#',
+              content:  'Terms of use',
+            },
+          ]
+        },
+        {
+          title: 'Language',
+          links:  [
+            {
+              url: '#',
+              content:  'English',
+            },
+            {
+              url: '#',
+              content:  'Polish',
+            }
+          ]
+        },
+        {
+          title: 'Follow us',
+          link: {
+            sourse: '#',
+            content:'./../../../assets/img/footer__in (2).svg',
+            description: 'footer_IN',
+          }
+        },
+        {
+          title: 'Contact',
+          link: [
+            {
+              url: 'hello@zen.com',
+              content: 'hello@zen.com',
+            }
+          ] 
+        },
+      ],
+      logo: {
+        image: {
+          sourse: './../../img/footer__logo.svg',
+          description:'footer__logo',
+        }
+      },
+      texts: [
+        'ZEN.COM is a licensed financial institution under the supervision of the Central Bank of Lithuania approved by European Banking Authority for 31 countries.',
+        'Registration number of the company 304749651, VAT ID LT100011714916.',
+        'License of electronic money institutions number LB000457.',
+        'Share capital 2.185.000,00 EUR.',
+        'Company address: Mėsinių g. 5, LT-01133, Vilnius, Lithuania.',
+        'Sales Office: ul. Emilii Plater 53, 00-113 Warsaw, Poland.',
+      ],
+      icon: {
+          image: './../../img/footer__icon.svg',
+          description:'footer__icon',
+      }
+    }
   },
-};
+  ru: {
+    header: {
+      logo:{
+        sourse: './../../img/logo.png',
+        description:'footer__icon',
+      },
+      menuItems: [
+        {
+          target: 'download',
+          content: 'Cкачать',
+          type: 'anchor'
+        },
+        {
+          target: 'warranty',
+          content: 'Гарантия',
+          type: 'anchor' 
+        },
+        {
+          target: 'care',
+          content: 'Забота',
+          type: 'anchor' 
+        },
+        {
+          target: 'cashback',
+          content: 'кэшбек',
+          type: 'anchor' 
+        },
+        {
+          target: 'clients',
+          content: 'Клиенты',
+          type: 'anchor' 
+        },
+      ],
+    },
+    download: {
+      name: 'скачать',
+      title: {
+        content: 'Безопасные покупки с заботой о деньгах',
+        priority: 1,
+      },
+      texts: [
+        'Узнайте, как мы можем помочь сделать ваш опыт покупок и управления деньгами более совершенным c ZEN.'
+      ],
+      links: {
+        googleLink: {
+          url: 'https://play.google.com',
+          description: 'Google ',
+        },
+        appleLink: {
+          url: 'https://www.apple.com/store',
+          description: 'apple',
+        },
+      image: {
+        source: './../../assets/img/1.jpg',
+        description: 'some description for image-1',
+      }
+  },
+ 
+    },
+    warranty: {
+      name: 'гарантии',
+      title: {
+        priority: 2,
+        content: 'Увеличение гарантии на 1 год',
+      },
+      texts: [
+        'Карты ZEN имеют очень удобную функцию, которая поможет вам, когда ваши электронные устройства будут выходить из строя после окончания стандартной гарантии, что происходит довольно часто.',
+        'При покупке любой электроники с помощью карты ZEN гарантия на них продлевается на один год. Как это работает? Просто. Автоматически.',
+      ],
+      image: {
+        sourse: './../../assets/img/2.jpg',
+        description: 'warranty__image',
+      }
+    },
+    care: {
+      name: 'Забота',
+      title: {
+        priority: 3,
+        content: 'Забота ZEN',
+      },
+      texts: [
+        'Покупки в Интернете, как правило, доставляют удовольствие. Но не всегда',
+        'Купленная вами вещь может оказаться бракованной, или совершенно не той, которую вы покупаете, и вам предстоит долгая процедура возврата товара.',
+        'Но, с картой ZEN Care: у вас бесплатная встроенная защита покупок.',
+      ],
+      image: {
+        source: './../../assets/img/3.jpg',
+        description: 'забота-ZEN',
+      }
+    },
+    cashback: {
+      name: 'Кэшбэк',
+      title: {
+        priority: 4,
+        content: 'Кэшбэк',
+      },
+      texts: ['Мы помогаем сэкономить деньги, пока вы делаете покупки, ведь зачастую требуется целая вечность, чтобы получить хоть какую-то выгоду.',
+              'Мы подумали и сделали кэшбэк более удобным.',
+              'До 14% от суммы, которую вы тратите с помощью ZEN Mastercard ®, будет зачислено на ваш счет сразу после покупки.'
+      ],
+      button: {
+        title:'Заказать консультацию',
+    },
+    clients: [  
+      {
+        image: './../../img/brands/Group-1.svg',
+      },
+      {
+        image: './../../img/brands/Group-2.svg',
+      },
+      {
+        image:  './../../img/brands/Group-3.svg',
+      },
+      {
+        image:  './../../img/brands/Group-4.svg',
+      },
+      {
+        image:  './../../img/brands/Group-5.svg',
+      },
+      {
+        image:  './../../img/brands/Group-6.svg',
+      },
+      {
+        image:  './../../img/brands/Group-7.svg',
+      },
+      {
+        image:  './../../img/brands/Group-8.svg',
+      }
+    ],
+    footer: {
+      columns: [
+        {
+          title: 'Learn',
+          links:  [
+            {
+              url: 'https://example.com',
+              content: 'Центр помощи'
+            },
+            {
+              url: '#',
+              content:  'Присоединяйтесь',
+            },
+            {
+              url: '#',
+              content: 'политика конфиденциальности'
+            },
+            {
+              url: '#',
+              content: 'Файлы cookie'
+            },
+            {
+              url: '#',
+              content: 'Правила пользования'
+            },
+          ]
+        },
+        {
+          title: 'Discover',
+          links:  [
+            {
+              url: '#',
+              content:  'Разработчикам',
+            },
+            {
+              url: '#',
+              content:  'Apple Pay',
+            },
+            {
+              url: '#',
+              content:  'Google Pay',
+            }
+          ]
+        },
+        {
+          title: 'Offer',
+          links:  [
+            {
+              url: '#',
+              content:  'Для бизнеса',
+            },
+            {
+              url: '#',
+              content:  'Для частных лиц',
+            }, 
+          ]
+        },
+        {
+          title: 'Язык',
+          links:  [
+            {
+              url: '#',
+              content:  'Английский',
+            },
+            {
+              url: '#',
+              content:  'Польский',
+            }
+          ]
+        },
+        {
+          title: 'Подписывайтесь на нас',
+          link: {
+            sourse: '#',
+            content:'./../../../assets/img/footer__in (2).svg',
+            description: 'footer_IN',
+          }
+        },
+        {
+          title: 'Контакты',
+          link: [
+            {
+              url: 'hello@zen.com',
+              content: 'hello@zen.com',
+            }
+          ] 
+        },
+      ],
+      logo: {
+        image: {
+          sourse: './../../img/footer__logo.svg',
+          description:'footer__logo',
+        }
+      },
+      texts: [
+        'ZEN.COM является лицензированным финансовым учреждением под надзором Центрального банка Литвы, одобренным Европейским банковским управлением для 31 страны.',
+        'Регистрационный номер компании 304749651, идентификатор НДС LT100011714916.',
+        'Лицензия учреждений по приему электронных денежных средств № LB000457.',
+        'Уставной капитал 2.185.000,00 евро.',
+        'Адрес компании: улица Мезиню, 5, LT-01133, Вильнюс, Литва.',
+        'Офис продаж: ул. Эмилия Платера 53, 00-113 Варшава, Польша.',
+      ],
+      icon: {
+          image: './../../img/footer__icon.svg',
+          description:'footer__icon',
+      }
+    },
+    },
+    clients:  {
+      name: 'clients',
+      title: {
+        priority: 5,
+        content: '',
+      },
+      clientsList: [
+        {
+          sourse: './../../img/brands/Group-1.svg',
+          description: 'RoseGal'
+        },
+        {
+          sourse:'./../../img/brands/Group-2.svg',
+          description:'GearBest',
+        } ,
+        {
+          sourse:'./../../img/brands/Group-3.svg',
+          description:'Alegro',
+        },
+        {
+          sourse:'./../../img/brands/Group-4.svg',
+          description: 'AliExpress',
+        },
+        {
+          sourse:'./../../img/brands/Group-5.svg',
+          description:'Booking.com',
+        } ,
+        {
+          sourse:'./../../img/brands/Group-6.svg',
+          description:'MediaMarkt',
+        },
+        {
+          sourse:'./../../img/brands/Group-7.svg',
+          description:'RTV',
+        },
+        {
+          sourse:'./../../img/brands/Group-8.svg',
+          description:'Groupon',
+        },
+      ],
+    },
+    footer: {
+      columns: [
+        {
+          title: 'Для учёбы',
+          links:  [
+            {
+              url: 'https://example.com',
+              content: 'Центр помощи'
+            },
+            {
+              url: '#',
+              content:  'Присоединяйтесь к команде ZEN',
+            },
+            {
+              url: '#',
+              content: 'Центр помощи'
+            },
+            {
+              url: '#',
+              content: 'Политика конфиденциальности'
+            },
+            {
+              url: '#',
+              content: 'Cookies'
+            },
+            {
+              url: '#',
+              content: 'Условия эксплуатации'
+            },
+          ]
+        },
+        {
+          title: 'Обнаружение',
+          links:  [
+            {
+              url: '#',
+              content:  'Для разработчиков',
+            },
+            {
+              url: '#',
+              content:  'Apple Pay',
+            },
+            {
+              url: '#',
+              content:  'Google Pay',
+            }
+          ]
+        },
+        {
+          title: 'Предложения',
+          links:  [
+            {
+              url: '#',
+              content:  'Бизнес',
+            },
+            {
+              url: '#',
+              content:  'Личные',
+            },
+            {
+              url: '#',
+              content:  'Политика конфиденциальности',
+            },
+            {
+              url: '#',
+              content:  'Cookies',
+            },
+            {
+              url: '#',
+              content:  'Условия эксплуатации',
+            },
+          ]
+        },
+        {
+          title: 'Язык',
+          links:  [
+            {
+              url: '#',
+              content:  'English',
+            },
+            {
+              url: '#',
+              content:  'Polski',
+            }
+          ]
+        },
+        {
+          title: 'Подписывайтесь на нас',
+          link: {
+            sourse: '#',
+            content:'./../../../assets/img/footer__in (2).svg',
+            description: 'footer_IN',
+          }
+        },
+        {
+          title: 'Contact',
+          link: [
+            {
+              url: 'hello@zen.com',
+              content: 'hello@zen.com',
+            }
+          ] 
+        },
+      ],
+      logo: {
+        image: {
+          sourse: './../../img/footer__logo.svg',
+          description:'footer__logo',
+        }
+      },
+      texts: [
+        'ZEN.COM является лицензированным финансовым учреждением под надзором Центрального банка Литвы, одобренным Европейским банковским управлением для 31 страны.',
+        'Регистрационный номер компании 304749651, VAT ID LT100011714916.',
+        'Номер лицензии учреждений электронных денег LB000457.',
+        'Уставной капитал2.185.000,00 EUR.',
+        'Адрес компании: Месиню ул. 5, LT-01133, Вильнюс, Литва.',
+        'Офис продаж: ул. Эмили Плятер 53, 00-113 Варшава, Польша.',
+      ],
+      icon: {
+          image: './../../img/footer__icon.svg',
+          description:'footer__icon',
+      }
+    }
+  }
+}
