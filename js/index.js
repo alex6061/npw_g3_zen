2ee45aadf15b49729a19107fda940065d938ac05
a.garkusha <a.garkusha@itcsolutions.ru>
2023-07-06 14:46:06 +0300

    Cleared code

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
 import { data } from './src/js/components/data.js';
 
-const { download,
-        warranty,
-        care,
-        cashback, 
-        clients, 
-        footer 
+const { 
+  download,
+  warranty,
+  care,
+  cashback, 
+  clients, 
+  footer 
 } = data.en;
 
 const root = document.querySelector('#root');
 
-root.insertAdjacentHTML('beforeend', Section (download));
-root.insertAdjacentHTML('beforeend', Section (warranty));
-root.insertAdjacentHTML('beforeend', Section (care));
-root.insertAdjacentHTML('beforeend', Section (cashback));
-root.insertAdjacentHTML('beforeend', Section (clients));
-root.insertAdjacentHTML('beforeend', Section (footer));
+root.insertAdjacentHTML('beforeend', Section(download));
+root.insertAdjacentHTML('beforeend', Section(warranty));
+root.insertAdjacentHTML('beforeend', Section(care));
+root.insertAdjacentHTML('beforeend', Section(cashback));
+root.insertAdjacentHTML('beforeend', Section(clients));
+root.insertAdjacentHTML('beforeend', Section(footer));
